5a56525cc74b9acfd504e11fb45733834d860e25
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-09 00:13:15 -0700

    'only capsys'

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tests the xonsh history."""
-# pylint: disable=protected-access
-import pytest
 
-
-@pytest.yield_fixture
-def hist():
-    yield 1
-
-
-CMDS = ['ls', 'cat hello kitty', 'abc', 'def', 'touch me', 'grep from me']
-
-@pytest.mark.parametrize('inp', [ (1,)])
-def test_show_cmd_numerate(inp, hist, xonsh_builtins, capsys):
+def test_show_cmd_numerate(capsys):
     """Verify that CLI history commands work."""
     pass
 
