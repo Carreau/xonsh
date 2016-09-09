4bf40f9608676226440b4593b3932e445c2d1d94
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-09 00:10:52 -0700

    'dummy fixture'

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
 """Tests the xonsh history."""
 # pylint: disable=protected-access
-import os
-import shlex
-
-from xonsh.history import History
-from xonsh import history
-
 import pytest
 
 
 @pytest.yield_fixture
 def hist():
-    #h = History(filename='xonsh-HISTORY-TEST.json', here='yup', sessionid='SESSIONID', gc=False)
     yield 1
-    #os.remove(h.filename)
 
 
 CMDS = ['ls', 'cat hello kitty', 'abc', 'def', 'touch me', 'grep from me']
 
-@pytest.mark.parametrize('inp, commands, offset', [
-    ('', CMDS, (0, 1)),
-    ])
-def test_show_cmd_numerate(inp, commands, offset, hist, xonsh_builtins, capsys):
+@pytest.mark.parametrize('inp', [ (1,)])
+def test_show_cmd_numerate(inp, hist, xonsh_builtins, capsys):
     """Verify that CLI history commands work."""
     pass
 
