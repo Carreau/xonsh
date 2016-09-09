95ce4b4870d71c708fe65d3f07f3663ca8ea9341
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-08 19:13:07 -0700

    'do skip'

--- tests/test_ptk_history.py
+++ tests/test_ptk_history.py
@@ -1,6 +1,10 @@
 import pytest
 
-#pytest.skip(msg='prompt_toolkit is not available')
 
+
+@pytest.skip(msg='prompt_toolkit is not available')
 def test_wrorks():
+    pass
+
+def test_wrorks_II():
     pass
